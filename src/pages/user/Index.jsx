3ab4e19910ecc8bcd48f4a71447ed00b4e693b23
Hasoon <144700795+Mohyi-Aldean@users.noreply.github.com>
2025-07-31 00:25:36 +0300

    Add user fetching with loading and error handling setUsers

--- src/pages/user/Index.jsx
+++ src/pages/user/Index.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Loader from "../../components/loader/Loader";
 
 function Index() {
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLodaing] = useState(true);
+
+  const getUsers = async () => {
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_BURL}/users`);
+      //console.log(response.data.users);
+      setUsers(response.data.users);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setIsLodaing(false);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  if (isLoading) return <Loader />;
+  if (error) return <p className="text-danger">{error.message}</p>;
+
   return (
-    <div>
-        <h2>Index</h2>
-    </div>
-  )
+    <>
+      {users.map((user) => (
+        <div className="user" key={user._id}>
+          <h2>{user.userName}</h2>
+        </div>
+      ))}
+    </>
+  );
 }
 
-export default Index
- 
+export default Index;
